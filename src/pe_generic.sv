a374ce84f6fbbc49c5bb05320d918594610ea997
Revanth Kumar Vattikula <138714366+revanthkumarvattikula@users.noreply.github.com>
2025-04-28 19:57:19 +0530

    Update pe_generic.sv

--- src/pe_generic.sv
+++ src/pe_generic.sv
@@ -1,11 +1,12 @@
+
 `timescale 1ns / 1ps
 module pe_generic(clk,reset,in_a,in_b,out_a,out_b,out_c);
 
- parameter data_size=8;
+ parameter data_size=4;
  input wire reset,clk;
- input wire [data_size-1:0] in_a,in_b;
- output reg [2*data_size:0] out_c;
- output reg [data_size-1:0] out_a,out_b;
+ input wire signed [data_size-1:0] in_a,in_b;
+ output reg signed [2*data_size:0] out_c;
+ output reg signed [data_size-1:0] out_a,out_b;
 
  always @(posedge clk)begin
     if(reset) begin
@@ -14,7 +15,7 @@ module pe_generic(clk,reset,in_a,in_b,out_a,out_b,out_c);
       out_c=0;
     end
     else begin  
-      out_c=out_c+in_a*in_b;
+      out_c=out_c + in_a * in_b;  // Signed multiplication
       out_a=in_a;
       out_b=in_b;
     end
